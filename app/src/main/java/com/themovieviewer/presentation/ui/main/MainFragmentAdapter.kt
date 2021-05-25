2bab319178078e7a185963efc3d10076254ff810
hanjh <hanjh@hancom.com>
2021-05-25 17:06:24 +0900

    implemented viewpager2

--- app/src/main/java/com/themovieviewer/presentation/ui/main/MainFragmentAdapter.kt
+++ app/src/main/java/com/themovieviewer/presentation/ui/main/MainFragmentAdapter.kt
@@ -3,8 +3,6 @@ package com.themovieviewer.presentation.ui.main
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.themovieviewer.presentation.ui.gallery.GalleryFragment
-import com.themovieviewer.presentation.ui.home.HomeFragment
 
 class MainFragmentAdapter(private val fa: FragmentActivity, private val fragments: List<Fragment>): FragmentStateAdapter(fa) {
 
@@ -14,10 +12,5 @@ class MainFragmentAdapter(private val fa: FragmentActivity, private val fragment
 
     override fun getItemCount() = PAGES
 
-    override fun createFragment(position: Int) =
-        if (position == 0) {
-            HomeFragment()
-        } else {
-            GalleryFragment()
-        }
+    override fun createFragment(position: Int) = fragments[position]
 }
