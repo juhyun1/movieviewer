101975762029f9a97fe7ba4776121afd9e8a0ed7
andrew <andrew.han@3i.ai>
2022-05-19 19:21:38 +0900

    migrating groovy to kotlin

---
+++ buildSrc/Versions.kt
@@ -0,0 +1,7 @@
+object Versions {
+    // gradle plugin
+    const val buildGradle = "4.0.1"
+    const val googleServicesGradle = "4.3.3"
+    const val firebaseCrashlyticsGradle = "2.2.0"
+
+}
---
+++ buildSrc/src/main/java/com/themovieviewer/Version.kt
@@ -0,0 +1,11 @@
+package com.themovieviewer
+
+object Versions {
+    // gradle plugin
+    const val buildGradle = "4.0.1"
+    const val GLIDE = "4.13.1"
+    const val KTX = "2.4.2"
+    const val KOTLIN_VERSION = "1.6.21"
+    const val RETROFIT = "2.9.0"
+
+}
