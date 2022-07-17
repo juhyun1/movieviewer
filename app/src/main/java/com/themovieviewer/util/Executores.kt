acf33c15d5b641f652cad16fc9a519a0c2d72b62
delight <delight6@gmail.com>
2022-07-18 02:24:01 +0900

    removed old fragments, repositories, dis, usecases

--- app/src/main/java/com/themovieviewer/util/Executores.kt
+++
@@ -1,9 +0,0 @@
-package com.themovieviewer.util
-
-import java.util.concurrent.Executors
-
-private val IO_EXECUTOR = Executors.newSingleThreadExecutor()
-
-fun ioThread(f: () -> Unit) {
-    IO_EXECUTOR.execute(f)
-}
--- core-data/src/main/java/com/themovieviewer/core/data/di/RepositoryModule.kt
+++ core-data/src/main/java/com/themovieviewer/core/data/di/RepositoryModule.kt
@@ -1,10 +1,16 @@
 package com.themovieviewer.core.data.di
 
+import com.google.gson.GsonBuilder
+import com.themovieviewer.core.data.network.MovieService
 import com.themovieviewer.core.data.repository.*
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 
 @InstallIn(SingletonComponent::class)
